35b4814e9428c8b465b71d4da2445534c8d51315
ARPIT287-CODER <arpitchauhan0808@gmail.com>
2026-09-04 14:17:30 +0530

    loop and funnction

---
+++ loops and fuctions.js
@@ -0,0 +1,63 @@
+// types of loop
+// for loop
+// for(let i=1;i<8;i++){
+//     console.log(i);
+// }
+// // for in loop
+// let a={
+//     name:"ram",
+//     class:2
+// }
+// let b=[1,2,3,4,5];
+
+// for (key in a) {
+//        console.log(key,":",a[key]);
+      
+// }
+// for(key in b){
+//     console.log(key," : ",b[key]);
+// }
+// for(key of b){
+//     console.log(key);
+
+// }
+// let i=0
+// while(i<5){
+//     console.log(i)
+//     i++;
+// }
+// let j=0
+// do{
+//      console.log(j)
+//     j++;
+// }
+// while(j>6);
+// function
+// let comparison=(a,b)=>{
+// if(a>b){
+//     return a;
+// }
+// else{
+//     return b;
+// }
+
+// }
+// console.log(comparison(2,5));
+// const evennumber=(n)=>{
+//     if(n%2==0){
+//         console.log("even number")
+//     }
+//     else{
+//         console.log("not a even number")
+//     }
+// }
+// evennumber(3);
+// let a={
+//     harry:55,
+//     peter:45,
+//     mathu:89
+// }
+// let name=Object.keys(a);
+// for(let i=0;i<name.length;i++){
+//     console.log(name[i]," : ",a[name[i]]);
+// }
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,21 @@
+for(let i=1;i<8;i++){
+//     console.log(i);
+// }
+// // for in loop
+// let a={
+//     name:"ram",
+//     class:2
+// }
+// let b=[1,2,3,4,5];
+
+// for (key in a) {
+//        console.log(key,":",a[key]);
+      
+// }
+// for(key in b){
+//     console.log(key," : ",b[key]);
+// }
+// for(key of b){
+//     console.log(key);
+
+// }

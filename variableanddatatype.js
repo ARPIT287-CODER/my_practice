f5f82ade6990edfeb7bb69d1060276651954dc63
ARPIT287-CODER <arpitchauhan0808@gmail.com>
2026-09-04 13:20:08 +0530

    variable

---
+++ variableanddatatype.js
@@ -0,0 +1,26 @@
+let a={
+}
+function data(d,b,c){
+    a.name=d;
+    a.phone_number=b;
+    a.marks=c;
+    console.log(a)
+}
+data("mohan",90864975,56)
+let e="1";
+console.log(typeof(e));
+const c={
+    name:"ram"
+}
+c.name="shyam"
+c.class=2;
+console.log(c)
+let dictionary={
+    red:"lal",
+    yellow:"pila",
+    greean:"hra",
+    pink:"gulabi",
+    water:"pani"
+}
+console.log(dictionary);
+console.log(dictionary.red)

d4941235c12fdf1ae0a09db65a22a0a7abd9d3b5
ARPIT287-CODER <arpitchauhan0808@gmail.com>
2026-09-04 14:42:10 +0530

    string

---
+++ string.js
@@ -0,0 +1,16 @@
+// function greet(a){
+// console.log(`good morning ${a}`);
+// }
+
+// greet("mohan")
+// let name=" mohan Das"
+// let name2='ramu'
+// console.log(name.length)
+
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(2,5))
+// console.log(name.replace("moh","han"))
+// console.log(name.concat(" ",name2))
+// console.log(name.trim())
+
